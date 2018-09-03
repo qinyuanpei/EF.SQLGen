d44cee31ae35721a5cb17e273656db996fc38566
qinyuanpei <qinyuanpei@163.com>
2018-09-03 17:38:15 +0800

    Update Code

---
+++ SQLGen.Core/Models/User.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace SQLGen.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string UserRole { get; set; }
+    }
+}
