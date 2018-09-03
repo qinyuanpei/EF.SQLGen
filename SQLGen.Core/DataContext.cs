660e9c4cc3352140f2f16fcb25d1289f59200d18
qinyuanpei <qinyuanpei@163.com>
2018-09-03 18:00:44 +0800

    Do it more

--- SQLGen.Core/DataContext.cs
+++ SQLGen.Core/DataContext.cs
@@ -1,9 +1,21 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using SQLGen.Models;
+using System;
 
 namespace SQLGen
 {
     public class DataContext: DbContext
     {
-        public new DbSet<TEntity>
+        public virtual DbSet<User> Users { get; set; }
+
+        public DataContext :base("")
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserTypeMap());
+        }
     }
 }
--- SQLGen.Core/Models/UserTypeMap.cs
+++ SQLGen.Core/Models/UserTypeMap.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace SQLGen.Models
 {
-    class UserTypeMap
+    public class UserTypeMap : IEntityTypeConfiguration<User>
     {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(e => e.UserId);
+        }
     }
 }
